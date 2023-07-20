47b6ad45e2cece935b6934dded0e1c1b9fcadc0b
ayazcis <ayazcisem@gmail.com>
2023-07-20 23:58:26 +0300

    sprint and hand anim
    sprint and hand animations added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,7 +6,15 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
 
+    public Animator leftHand;
+    public Animator rightHand;
+
+
+
     public float speed = 12f;
+    public float tempSpeed;
+    public float runSpeed = 12f;
+
     public float gravity = -9.81f;
     public float jump = 1f;
 
@@ -17,9 +25,15 @@ public class PlayerMovement : MonoBehaviour
     Vector3 velocity;
     bool isGrounded;
 
+	private void Start()
+	{
+        tempSpeed = speed;
+
+    }
     // Update is called once per frame
     void Update()
     {
+        speed = tempSpeed;
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         if (isGrounded && velocity.y < 0)
@@ -31,9 +45,21 @@ public class PlayerMovement : MonoBehaviour
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
+		if (Input.GetKey(KeyCode.LeftShift))
+		{
+            tempSpeed = speed;
+            speed = runSpeed;
+            rightHand.SetBool("run", true);
+            leftHand.SetBool("run", true);
 
+		}
+		else
+		{
+            rightHand.SetBool("run", false);
+            leftHand.SetBool("run", false);
+        }
+ 
         controller.Move(move * speed * Time.deltaTime);
-        Debug.Log(isGrounded);
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jump * -2f * gravity);
@@ -42,6 +68,19 @@ public class PlayerMovement : MonoBehaviour
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
+        Debug.Log(x);
+		if (x!= 0 || z!=0)
+		{
+            leftHand.SetBool("move", true);
+            rightHand.SetBool("move", true);
+		}
+		else
+		{
+            leftHand.SetBool("move", false);
+            rightHand.SetBool("move", false);
+
+        }
+
     }
 }
 
